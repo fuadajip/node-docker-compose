3c60100aac601b70b915a04ac7d42e74cb4946b7
fuadajip <fuadajipratomo@gmail.com>
2017-08-13 22:24:36 +0700

    Add endpoint

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ app.get('/', (req, res) => {
     res.send('Hi, my name fuadajip');
 });
 
+app.get('/tuna', (req, res) => {
+    res.send('I\'m ordering some tuna');
+})
+
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
